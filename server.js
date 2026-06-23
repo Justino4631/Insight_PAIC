e13660cc58c0c480dcf3089a96fe9b2e1eb8cc74
Justin Baratta <Justin.M.Baratta@outlook.com>
2026-06-23 15:52:01 -0700

    Use express.static('public')

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(express.static("public"));
 
 app.get("/api/message", (req, res) => {
     res.json({message: "Hello from the Node.js backend!"});
 });
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
